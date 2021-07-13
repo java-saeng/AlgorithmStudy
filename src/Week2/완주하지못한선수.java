be04eaf7ee3bcd3c6a0cc47bff3b6d4415077f17
user <gusdn85554@naver.com>
2021-07-14 05:38:33 +0900

    Programmers 완주하지 못한 선수

---
+++ src/Week2/완주하지못한선수.java
@@ -0,0 +1,36 @@
+package Week2;
+
+import java.util.*;
+
+class 완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        String answer = "";
+
+        for(int i = 0; i < participant.length; i++){
+            String s = participant[i];
+            if(map.containsKey(s)){
+                int value = map.get(s);
+                value++;
+                map.put(s,value);
+            }
+            else{
+                map.put(s, 1);
+            }
+        }
+
+        for(int i = 0; i < completion.length; i++){
+            String s = completion[i];
+            int value = map.get(s);
+            value--;
+            map.put(s,value);
+        }
+
+        for(String key : map.keySet()){
+            if(map.get(key) != 0)
+                answer = key;
+        }
+
+        return answer;
+    }
+}
