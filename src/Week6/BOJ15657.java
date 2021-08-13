3f17454a0388f364c957369ac45d5a83b0c97591
hyeonwoo <gusdn85554@naver.com>
2021-08-13 16:00:12 +0900

    BOJ 15657

---
+++ src/Week6/BOJ15657.java
@@ -0,0 +1,50 @@
+package Week6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ15657 {
+    static int atoi(String str) {
+        return Integer.parseInt(str);
+    }
+    static int arr[];
+    static int ar[] = new int[9];
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = atoi(st.nextToken());
+        M = atoi(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = atoi(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0, 0);
+        System.out.println(sb);
+
+
+    }
+
+    static void dfs(int start, int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ar[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            ar[cnt] = arr[i];
+            dfs(i, cnt + 1);
+        }
+    }
+}
