8bcc9d2d1f842498c69f08b8c5f867a5d7d4bd26
hyeonwoo <gusdn85554@naver.com>
2021-08-23 09:31:11 +0900

    프로그래머스

---
+++ src/Week7/짝지어제거하기.java
@@ -0,0 +1,38 @@
+package Week7;
+
+import java.util.*;
+
+class 짝지어제거하기 {
+    public int solution(String s){
+        int answer = -1;
+        Stack<Character> stack = new Stack<>();
+
+        char ch[] = new char[s.length()];
+
+        for(int i = 0; i < s.length(); i++){
+            ch[i] = s.charAt(i);
+        }
+
+        int idx = 0;
+        while(idx != s.length()){
+            if(stack.isEmpty()) {
+                stack.push(ch[idx]);
+                idx++;
+            }
+            else {
+                if(stack.peek() == ch[idx]){
+                    stack.pop();
+                }
+                else    {
+                    stack.push(ch[idx]);
+                }
+                idx++;
+            }
+        }
+
+        if(stack.isEmpty()) answer = 1;
+        else answer = 0;
+
+        return answer;
+    }
+}
